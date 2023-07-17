75168297c4c4bc3f4199d1fdc4d363bedd779700
Sourav halder <souravh093@gmail.com>
2023-07-17 17:19:19 +0600

    add dark mode in my app

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -5,7 +5,9 @@ import React from 'react';
 const Providers = ({children}) => {
     return (
         <ThemeProvider enableSystem={true} attribute='class'>
+            <div className='dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen select-none'>
             {children}
+            </div>
         </ThemeProvider>
     );
 };
--- src/components/DarkModsSwitch.jsx
+++ src/components/DarkModsSwitch.jsx
@@ -1,29 +1,29 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { MdLightMode } from "react-icons/md";
 import { BsMoonFill } from "react-icons/Bs";
 import { useTheme } from "next-themes";
 
 const DarkModsSwitch = () => {
   const { systemTheme, theme, setTheme } = useTheme();
-  useEffect(() => {
-    setMounte
-  }, [])
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
   const currentTheme = theme === "system" ? systemTheme : theme;
   return (
     <>
-      {currentTheme === "dark" ? (
-        <MdLightMode
-          className="text-xl cursor-pointer hover:text-amber-500"
-          onClick={() => setTheme("light")}
-        />
-      ) : (
-        <BsMoonFill
-          className="text-xl cursor-pointer hover:text-amber-500"
-          onClick={() => setTheme("dark")}
-        />
-      )}
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            className="text-xl cursor-pointer hover:text-amber-500"
+            onClick={() => setTheme("light")}
+          />
+        ) : (
+          <BsMoonFill
+            className="text-xl cursor-pointer hover:text-amber-500"
+            onClick={() => setTheme("dark")}
+          />
+        ))}
     </>
   );
 };
