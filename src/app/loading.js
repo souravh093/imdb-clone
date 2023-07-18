bc4c8aa56fd30d6ce75d8dc8f1f288b6e4d097ae
Sourav halder <souravh093@gmail.com>
2023-07-18 18:04:54 +0600

    add card compontens

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='flex justify-center items-center'>
+           <img className='h-96' src="loader.svg" alt="loading..." />
+        </div>
+    );
+};
+
+export default loading;
